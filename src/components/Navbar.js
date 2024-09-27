3fb9ae981ce1582b57aa32a34b12949c25f6be63
nikhilrajraput <nikhilsinghraj11@gmail.com>
2024-09-27 15:44:15 +0530

    updated navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-
+import './Navbar.css'
 const Navbar = () => {
   const cartCount = useSelector(state => state.cart.cart.length);
 
